b9c2906adaef283cd36c54abeb7c4267f0f07300
Tatiana Muhidaeva <batagaeva.ta@gmail.com>
2020-01-19 23:02:05 +0800

    ready

---
+++ build/assets/js/main.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+
+
+  
+});
+$(document).ready(function () {
+ $('#toggle').on('click', function(){
+   $('.header-nav').toggleClass('open');
+ });
+ $(document).on('mouseup', function (e){ 
+  var menu = $(".header-nav.open");
+  if (!menu.is(e.target)
+      && menu.has(e.target).length === 0
+      && !$('#toggle').is(e.target)) { 
+    menu.removeClass('open'); 
+  }
+});
+
+})
+$(document).ready(function () {
+
+    var mainSlider = new Swiper('.main-swiper-container', {
+      slidesPerView: 1,
+      speed: 500,
+      navigation: {
+        prevEl: '.main-swiper-arrow--prev',
+        nextEl: '.main-swiper-arrow--next',
+      },
+    });
+    var servicesSlider = new Swiper('.services-swiper-container', {
+      slidesPerView: 1,
+      initialSlide: 1,
+      centeredSlides: false,
+      speed: 500,
+      loop: true,
+      navigation: {
+        prevEl: '.services-swiper-arrow--prev',
+        nextEl: '.services-swiper-arrow--next',
+      },
+      breakpoints: {
+        767: {
+          slidesPerView: 3,
+          centeredSlides: true,
+        },
+        575: {
+          slidesPerView: 2,
+          centeredSlides: false,
+        },
+      }
+    });
+    var feedbackThumbs = new Swiper('.feedback-thumbs-container', {
+      spaceBetween: 16,
+      slidesPerView: 2,
+      breakpoints: {
+        575: {
+          slidesPerView: 3,
+        },
+      }
+    });
+    var feedbackTop = new Swiper('.feedback-top-container', {
+      navigation: {
+        nextEl: '.feedback-top-arrow--next',
+        prevEl: '.feedback-top-arrow--prev',
+      },
+      thumbs: {
+        swiper: feedbackThumbs
+      },
+      on: {
+        slideChange: function () {
+          feedbackTop.thumbs.swiper.slideToLoop(feedbackTop.realIndex);
+        }
+      },
+    });
+});
